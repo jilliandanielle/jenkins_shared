0a3cdbbc5877d40f493db90a2eff55bb95cca206
jilliandanielle <jilliandanielle@gmail.com>
2020-10-22 14:17:58 +0100

    Update replaceString.groovy

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,5 +1,6 @@
-def call(){
-     def text = readFile file: "index.html"
+def call(String fileName){
+     def text = readFile file: "${fileName}"
      text = text.replaceAll("%BUILD_NUMBER%","${BUILD_NUMBER}" )
-     writeFile file: "index.html", text: text
+     writeFile file: "${fileName}", text: text
  }
+
